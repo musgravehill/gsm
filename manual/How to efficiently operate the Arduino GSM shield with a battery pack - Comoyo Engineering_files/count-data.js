cbcc9a56888e267720d259f3e2660c72e50b315c
musgravehill <musgravehill@gmail.com>
2015-11-13 14:08:13 +0400

    info

---
+++ manual/How to efficiently operate the Arduino GSM shield with a battery pack - Comoyo Engineering_files/count-data.js
@@ -0,0 +1,5 @@
+var DISQUSWIDGETS;
+
+if (typeof DISQUSWIDGETS != 'undefined') {
+    DISQUSWIDGETS.displayCount({"text":{"and":"and","comments":{"zero":"0 Comments","multiple":"{num} Comments","one":"1 Comment"}},"counts":[{"id":"http:\/\/comoyo.github.io\/blog\/2013\/08\/09\/How_to_efficiently_operate_the_Arduino_GSM_shield_with_a_battery_pack\/","comments":6}]});
+}
